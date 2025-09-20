6a067022147faf129173ccad3007e31beb3b0d80
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2025-09-20 10:31:47 -0600

    feat: auth

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,20 @@
+<script setup>
+const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession()
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div v-if="loggedIn">
+    <h1>Welcome!</h1>
+    <pre>{{ user }}</pre>
+    <pre>{{ session }}</pre>
+    <button @click="clear">Logout</button>
+  </div>
+  <div v-else>
+    <h1>Not logged in</h1>
+    <a href="/auth/strava">Login with Strava</a>
+    <!-- or open the OAuth route in a popup -->
+    <button @click="openInPopup('/auth/strava')">Login with Strava</button>
   </div>
 </template>
+
---
+++ server/routes/auth/strava.get.ts
@@ -0,0 +1,26 @@
+export default defineOAuthStravaEventHandler({
+  config: {
+    approvalPrompt: 'force',
+    scope: ['profile:read_all', 'activity:read_all'],
+  },
+  async onSuccess(event, { user, tokens }) {
+    await setUserSession(event, {
+      user: {
+        user
+      },
+      secure: {
+        apiToken: tokens.access_token,
+        expiresAt: tokens.expires_at,
+        expiresIn: tokens.expires_in,
+        refreshToken: tokens.refresh_token
+      },
+      loggedInAt: new Date()
+    })
+    return sendRedirect(event, '/')
+  },
+  // Optional, will return a json error and 401 status code by default
+  onError(event, error) {
+    console.error('GitHub OAuth error:', error)
+    return sendRedirect(event, '/')
+  },
+})
